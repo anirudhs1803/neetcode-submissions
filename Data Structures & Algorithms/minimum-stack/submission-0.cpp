4f9b9432ec46b16e474defa56f0f1e9ae4007567
anirudhs1803 <159309020+anirudhs1803@users.noreply.github.com>
2026-08-08 17:50:06 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }
+        else{
+            int currentmin = min(val, st.top().second);
+            st.push({val, currentmin});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
